bd2414cb92f728cc5d30f3c9182145da9c97a9eb
Tarun <tarun.bhola13@gmail.com>
2021-07-11 18:13:46 +0530

    Service Tests Updated

--- src/test/java/com/tarun/testdeployment/service/EmployeeServiceImplTest.java
+++ src/test/java/com/tarun/testdeployment/service/EmployeeServiceImplTest.java
@@ -1,28 +1,67 @@
 package com.tarun.testdeployment.service;
 
-import org.junit.jupiter.api.Test;
-
+import com.tarun.testdeployment.entity.EmployeeDTO;
+import com.tarun.testdeployment.response.RequestException;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EmployeeServiceImplTest {
 
+    @Autowired
+    private EmployeeServiceImpl employeeService;
+
+    static EmployeeDTO employeeDTO;
+
+    @BeforeAll
+    static void initialize(){
+        employeeDTO = new EmployeeDTO();
+        employeeDTO.setEmail("test@email.com");
+        employeeDTO.setFirstName("First");
+        employeeDTO.setLastName("Test");
+    }
     @Test
-    void save() {
+    @Order(1)
+    void save() throws RequestException {
+
+        int id = employeeService.save(employeeDTO);
+        Assertions.assertTrue(id != 0);
+        employeeDTO.setId(id);
     }
 
     @Test
-    void findById() {
+    @DisplayName("Find By Id Method")
+    @Order(2)
+    void findById() throws RequestException {
+
+        Optional<EmployeeDTO> employee = employeeService.findById(employeeDTO.getId());
+        assertTrue(employee.isPresent());
     }
 
     @Test
-    void deleteById() {
+    @Order(4)
+    void deleteById() throws RequestException{
+
+        EmployeeDTO employee = employeeService.deleteById(employeeDTO.getId());
+        assertTrue(employee instanceof EmployeeDTO);
     }
 
     @Test
-    void updateEmployee() {
+    @Order(3)
+    void updateEmployee() throws RequestException {
+
+       EmployeeDTO employee =  employeeService.updateEmployee(employeeDTO);
+       assertTrue(employee.getId() == employeeDTO.getId());
     }
 
     @Test
-    void findAll() {
+    @Order(5)
+    void findAll() throws RequestException{
+
+        assertTrue(employeeService.findAll().size() > 0);
     }
 }
